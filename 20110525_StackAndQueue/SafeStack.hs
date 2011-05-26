c4ed18b627148461ce2883b78d8ff6a862b1b9cd
Kiwamu Okabe <kiwamu@debian.or.jp>
2011-05-26 18:06:59 +0900

    after meeting

--- 20110525_StackAndQueue/SafeStack.hs
+++ 20110525_StackAndQueue/SafeStack.hs
@@ -1,12 +1,12 @@
-module SafeStack (SafeStack(..), SimpleSafeStack(..)) where
+module SafeStack (SafeStack(..), SimpleSafeStack) where
 import Prelude hiding (head, tail)
 
 class SafeStack s where
   empty   :: s a
-  isEmpty :: (s a) -> Bool
-  cons    :: a -> (s a) -> (s a)
-  head    :: (s a) -> Maybe a
-  tail    :: (s a) -> Maybe (s a)
+  isEmpty :: s a -> Bool
+  cons    :: a -> s a -> s a
+  head    :: s a -> Maybe a
+  tail    :: s a -> Maybe (s a) -- Maybeとsがくっつかないようにカッコつける!
 
 data SimpleSafeStack a = SSS [a]
                      deriving (Eq)
@@ -15,7 +15,7 @@ instance SafeStack SimpleSafeStack where
   empty = SSS []
   isEmpty (SSS ss) = null ss
   cons x (SSS ss) = SSS (x : ss)
-  head (SSS []) = Nothing
   head (SSS (x:xs)) = Just x
-  tail (SSS []) = Nothing
+  head (SSS []) = Nothing
   tail (SSS (x:xs)) = Just (SSS xs)
+  tail (SSS []) = Nothing
